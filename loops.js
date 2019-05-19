28a2bb414fcb07164194f3323a1f3dbc9bf92a77
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-19 18:35:27 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -32,7 +32,7 @@ function incrementVariable() {
 }
 
 function doWhileLoop(num) {
-  var counter = 1;
+  // var counter = 1;
   do {
     console.log("I run once regardless.");
     // console.log(counter);
