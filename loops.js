6d0fc6f4745f312257131033d1c9c7b1748fb69d
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-19 18:36:28 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -35,6 +35,7 @@ function doWhileLoop(num) {
   // var counter = 1;
   do {
     console.log("I run once regardless.");
+    console.log("I run once regardless. ${i}");
     // console.log(counter);
     // counter += 1;
   } while (incrementVariable() < num);
