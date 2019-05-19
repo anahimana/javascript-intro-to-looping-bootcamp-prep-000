97aebc0e7c58fa24312336e41a74a43a25c8046f
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-19 18:23:42 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,41 @@
+function forLoop(array) {
+  for(let x = 0; x < 25; x++){
+    if(x > 1){
+      array.push(`I am ${x} strange loops.`);
+    }else{
+      array.push(`I am ${x} strange loop.`);
+    }
+  }
+  
+  return array;
+}
+
+const myArray = [];
+// console.log(forLoop(myArray));
+
+function whileLoop(number) {
+  counter = number;
+  while(counter > 0) {
+    counter -= 1;
+    console.log(counter);
+  }
+  return "done";
+}
+
+// console.log(whileLoop(5));
+
+var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+function doWhileLoop(number) {
+  do {
+    console.log("doo-bee-doo-bee-doo");
+  } while (incrementVariable() < 5);
+}
+
+
+
